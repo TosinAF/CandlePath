078bd6d95ba34243fb6ddcdcc7d330e75cb5c153
Tosin Afolabi <psyooa@nottingham.ac.uk>
2013-10-12 15:19:22 +0100

    Initial Commit

---
+++ Candle Path/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Candle Path
+//
+//  Created by Tosin Afolabi on 12/10/2013.
+//  Copyright (c) 2013 Tosin Afolabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPAppDelegate class]));
+    }
+}
